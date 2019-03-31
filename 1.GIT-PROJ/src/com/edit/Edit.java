079828fabb4a5113d449c31ade7b711a9b84f6f8
Dell <Dell@192.168.100.16>
2019-03-31 17:41:12 +0530

    add comment

---
+++ 1.GIT-PROJ/src/com/edit/Edit.java
@@ -0,0 +1,9 @@
+package com.edit;
+
+public class Edit {
+
+	public static void main(String[] args) {
+		System.out.println("Succeed");
+	}
+
+}
--- 1.GIT-PROJ/src/com/git/DummyGit.java
+++ 1.GIT-PROJ/src/com/git/DummyGit.java
@@ -3,8 +3,6 @@ package com.git;
 public class DummyGit {
 	
 	public static void main(String [] args) {
-		System.out.println("Dummy Project edit");
+		System.out.println("Dummy Project edit");//congrest
 	}
-
-
 }
